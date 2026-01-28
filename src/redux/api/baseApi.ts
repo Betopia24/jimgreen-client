7b17bdc64ad0ca91e51789dbf8b461cd68eae2ce
Bikash sarker <bikash.webdeveloper5@gmail.com>
2026-01-28 17:02:39 +0600

    the row matrials create api intregration

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -8,8 +8,9 @@ export const baseApi = createApi({
     credentials: "include",
     prepareHeaders: (headers) => {
       const token = Cookies?.get("token");
+
       if (token) {
-        headers.set("Authorization", `${token}`);
+        headers.set("Authorization", `Bearer ${token}`);
       }
       return headers;
     },
