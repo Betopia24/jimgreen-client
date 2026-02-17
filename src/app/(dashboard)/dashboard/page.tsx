6068531b5dd85aa6d970ce5159048788dbd786dd
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-02-17 15:36:00 +0600

    the change member home add loading

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,19 @@
+"use client";
 import AnalysisTrends from "@/components/dashboard/dashboard/AnalysisTrends";
 import LatestReportTable from "@/components/dashboard/dashboard/latestRepostTable";
-import LatestRepostTable from "@/components/dashboard/dashboard/latestRepostTable";
+
 import StatsCards from "@/components/dashboard/dashboard/StatsCards";
 
+import LoadingPage2 from "@/components/shared/loading/LoadingPage2";
+import { useGetHomeOverviewQuery } from "@/redux/api/home/homeSlicsApi";
+
 function Dashboard() {
+  const { data: statsData, isLoading } = useGetHomeOverviewQuery("");
+
+  if (isLoading) {
+    return <LoadingPage2 />;
+  }
+
   return (
     <div>
       {/* card  */}
