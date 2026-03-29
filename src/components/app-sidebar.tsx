f86e68bef484e2e10dbfd843c7dc9ba3c55cd20c
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-03-29 17:17:59 +0600

    the saturation route create

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -22,6 +22,7 @@ import { LuLoader } from "react-icons/lu";
 import { useState } from "react";
 import { PiMagicWandLight } from "react-icons/pi";
 import {
+  Box,
   Building,
   FileInput,
   GlassWater,
@@ -49,9 +50,9 @@ export default function AppSidebar() {
       size: 16,
     },
     {
-      href: "/dashboard/analysisInput",
-      icon: FileInput,
-      label: "Analysis Input",
+      href: "/dashboard/saturation",
+      icon: Box,
+      label: "Saturation Analysis",
       size: 16,
     },
     {
