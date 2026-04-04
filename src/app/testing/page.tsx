47751f9c2ca48a0c6bb7e8fdbbd552e3490c8860
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-04-04 11:20:08 +0600

    the saturation

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -2,11 +2,7 @@ import { graphData } from "./__comp/graphData";
 import SaturationDashboard from "./__comp/ThreeD";
 
 const page = () => {
-  return (
-    <div>
-      <SaturationDashboard apiResponse={graphData} />
-    </div>
-  );
+  return <div>{/* <SaturationDashboard apiResponse={graphData} /> */}</div>;
 };
 
 export default page;
